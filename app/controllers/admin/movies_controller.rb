30593554680761dd740863d474b3a82a97f2d27b
Kumagai <obama0419@gmail.com>
2025-03-26 08:34:08 +0900

    作業04対応

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,22 +1,37 @@
-module Admin
-  class MoviesController < ApplicationController
-    def new
-      @movie = Movie.new
-    end
+class Admin::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
 
-    def create
-      @movie = Movie.new(movie_params)
-      if @movie.save
-        redirect_to admin_movies_path
-      else
-        render :new
-      end
-    end
+  def new
+    @movie = Movie.new
+  end
 
-    private
-
-    def movie_params
-      params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to admin_movies_path
+    else
+      render :new
     end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to admin_movies_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root "movies#index" # ← これはそのままでOK
+
   namespace :admin do
-    resources :movies, only: [:index, :new, :create]
+    resources :movies, only: [:index, :new, :create, :edit, :update] # ← ここに :edit, :update を追加
   end
+
+  resources :movies, only: [:index] # ← root に指定するには必要
 end
