6aaed872207df26cb0ee55743e5ee653ebbfd7f7
Kumagai <obama0419@gmail.com>
2025-04-10 14:29:38 +0900

    作業06対応

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,16 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all  # データベースからすべての映画を取得
+    @movies = Movie.all
+
+    # 検索キーワードがあればフィルタリング
+    if params[:keyword].present?
+      @movies = @movies.where("name LIKE ? OR description LIKE ?", "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+    end
+
+    # 公開状態の絞り込み（true/false）
+    if params[:is_showing].present?
+      is_showing = ActiveModel::Type::Boolean.new.cast(params[:is_showing])
+      @movies = @movies.where(is_showing: is_showing)
+    end
   end
 end
